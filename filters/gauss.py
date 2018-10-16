f14d87e88928d00cfc862f828b07cebd7752feb4
Nicolas <ndeleon@itba.edu.ar>
2018-10-15 21:39:56 -0300

    Gauss fijarse tao,devuelve polos y zeros

--- filters/gauss.py
+++ filters/gauss.py
@@ -27,5 +27,37 @@ class Gauss(base_filter):
             self.do_approximation()#Does normalized approximation and realizes
             self.denormalize()#Denormalizes the approximation to match desired template
 
+    
+    def factorial(self,n):
+        if n <= 0:
+            return 1
+        else:
+            return n*self.factorial(n-1)
+
     def do_approximation(self):
-       pass
+        #Me pasan Tao(0) WRG y palmerita
+        wrgn = self.tao0*self.wrg
+        j = np.complex(0,1)
+        NoCumplo=True
+        self.n=1
+        
+        while NoCumplo or self.n<=20:
+            Terminos= np.zeros(self.n*2)
+            Terminos[len(Terminos)]=1
+           
+            for i in range (0,self.n+1):
+                Terminos[len(Terminos)-2*i] = (-1*(j**i))/self.factorial(i)
+
+            POL=np.poly1d(Terminos)
+            den=Terminos
+            w,TaoWRGN = signal.group_delay((1,den),[wrgn])
+
+            if TaoWRGN >= (1-self.palm):
+                NoCumplo=False
+            else:
+                NoCumplo=True
+
+            self.poles=np.roots(POL)
+            self.zeroes=1
+            self.n=self.n+1
+                
